a45fb36bc9dcaf87ebe9d6953e5d3ed3f395ef2d
worldii <bbc2788@naver.com>
2023-05-09 16:05:52 +0900

    complete 0509 workshop : vue project

---
+++ book_workshop/src/views/BookView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+    <h1 class="underline">SSAFY 도서 정보</h1>
+            <div class="regist_form" >
+              <label for="isbn">도서번호</label>
+              <div class="view">{{ book.isbn }}</div>
+              <label for="title">도서명</label>
+              <div class="view">{{ book.title }}</div>
+              <label for="author">저자</label>
+              <div class="view">{{ book.author }}</div>
+              <label for="price">가격</label>
+              <div class="view">{{ book.price }}원</div>
+              <label for="content">설명</label>
+              <div class="view">{{ book.desc }}</div>
+              <div style="padding-top: 15px">
+                <a href="#" class="btn" @click="modifyBook">수정</a>
+                <a href="#" class="btn" @click="deleteBook">삭제</a>
+                <a href="./list.html" class="btn">목록</a>
+              </div>
+            </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'BookView',
+    
+    props: {
+      book: Object,
+    
+    },
+
+    methods: {
+        deleteBook() {
+            location.href = "/work/delete.html?isbn=" + this.book.isbn;
+        },
+        modifyBook() {
+            location.href = "/work/modify.html?isbn=" + this.book.isbn;
+        },
+    },
+};
+</script>
+
+<style scoped></style>
---
+++ stop-watch/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+      <div>time: {{ time }}</div>
+      <button @click="start">start</button>
+      <button @click="stop">stop</button>
+      <button @click="reset">reset</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+      return {
+        time: 0,
+        timer: null,
+      };
+    },
+    methods: {
+      start() {
+        //let that = this;
+
+        // arrow function을 사용하면 this가 Vue instance를 가리키게 된다.
+        this.timer = setInterval(() => {
+          this.time++;
+        }, 1000);
+        // 만약 arrow function을 사용하지 않으면 this가 window를 가리키게 된다.
+        // this.timer = setInterval(function () {
+        //  console.log(that);
+        //  that.time++;
+        //  해결 방법 : that = this; 를 사용한다.
+        //   this.time++;
+        // }, 1000);
+      },
+      stop() {
+        clearInterval(this.timer);
+      },
+      reset() {
+        this.time = 0;
+      },
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
