a6caabeed17775777d45834e89a77462982febbd
worldii <bbc2788@naver.com>
2023-05-09 11:16:36 +0900

    Feat : Vue board project list article

---
+++ board-vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+
+    <the-header></the-header>
+  
+    <app-board></app-board>
+  </div>
+</template>
+
+
+<script>
+
+import TheHeader from './components/TheHeader.vue';
+import AppBoard from './views/AppBoard.vue';
+export default ({
+    components: {
+      TheHeader,
+      AppBoard
+    }
+  });
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
