f2ee82b1d1d5f2234d8490ad9f7e0c496f88bac1
worldii <bbc2788@naver.com>
2023-05-09 09:54:13 +0900

    Docs : Readme.md 수정

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Vue와 관련하여, SSAFY에서 공부한 것들을 모아놓는 공간입니다.
 - [x] ES6 문법
 - [x] Vue 인스턴스, Directive
+- [x] Vue 객체, 이벤트 처리
+- [x] Vue 프로젝트 설정
---
+++ npm-test/index.js
@@ -0,0 +1,7 @@
+var prompt = require('prompt-sync')();
+
+//
+// get input from the user.
+//
+
+var n = prompt('How many more times? ');
